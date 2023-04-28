21d6cf15f9fe3c8e635862d3d5e603df5ebd4372
krugloffix <krugloffix@gmail.com>
2023-04-28 11:13:12 +0700

    add navigation
    Добавила навигацию и скрины

---
+++ screens/ButtonScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const ButtonScreen = () => {
+  return <Text>ButtonScreen</Text>;
+};
+
+export default ButtonScreen;
---
+++ screens/GroupScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const GroupScreen = () => {
+  return <Text>GroupScreen</Text>;
+};
+
+export default GroupScreen;
